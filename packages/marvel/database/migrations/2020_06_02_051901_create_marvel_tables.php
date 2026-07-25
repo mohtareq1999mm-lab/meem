4befbc4f7cdf94e0a227c1a21455adcec6afe161
mohtareq1999mm-lab <mohtareq1999mm@gmail.com>
2026-07-25 14:49:22 +0300

    feat(migration): add minimum order amount column to marvel tables

--- packages/marvel/database/migrations/2020_06_02_051901_create_marvel_tables.php
+++ packages/marvel/database/migrations/2020_06_02_051901_create_marvel_tables.php
@@ -396,6 +396,7 @@ class CreateMarvelTables extends Migration
             $table->string('promotion_video_url')->nullable();
             $table->string('youtube')->nullable();
             $table->string('phone')->nullable();
+            $table->decimal('minimum_order_amount', 10, 2)->default(0);
             $table->boolean('fast_shipping_page_publish')->default(true);
             $table->json('options')->nullable();
             $table->timestamps();
